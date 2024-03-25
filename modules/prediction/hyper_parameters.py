ea54516031823ccf5a1d12e4c05d126c3f2e9afb
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-03-25 17:51:32 +0800

    (adjust): tuples

--- modules/prediction/hyper_parameters.py
+++ modules/prediction/hyper_parameters.py
@@ -4,8 +4,10 @@ from torch import optim
 HIDDEN_NODES = (5, 8, 11)
 ACTIVATION_FUNCTIONS = (nn.Tanh, nn.ReLU)
 WEIGHT_INITIALIZERS = (nn.init.normal_, nn.init.xavier_normal_, nn.init.kaiming_normal_)
-LOSS_FUNCTIONS = (nn.functional.mse_loss)
+LOSS_FUNCTIONS = (nn.functional.mse_loss,)
 REGULARIZATION_FACTORS = (0.001, 0.0001)
 OPTIMIZERS = (optim.SGD, optim.Adam)
 LEARNING_EPOCHS = (100, 200, 300)
-
+LEARNING_RATE_SCHEDULERS = (None, optim.lr_scheduler.CosineAnnealingLR)
+EMSEMBLE_COUNTS = (5,)
+NORMALIZERS = (nn.BatchNorm1d,)
