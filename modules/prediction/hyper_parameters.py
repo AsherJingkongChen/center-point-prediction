f2e52ac91f6c80483b9f2c41722bf76aea05d546
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-03-25 18:16:11 +0800

    (refactor): Wrap hyper params in a dataclass

--- modules/prediction/hyper_parameters.py
+++ modules/prediction/hyper_parameters.py
@@ -1,13 +1,36 @@
-from torch import nn
+from dataclasses import dataclass
 from torch import optim
 
-HIDDEN_NODES = (5, 8, 11)
-ACTIVATION_FUNCTIONS = (nn.Tanh, nn.ReLU)
-WEIGHT_INITIALIZERS = (nn.init.normal_, nn.init.xavier_normal_, nn.init.kaiming_normal_)
-LOSS_FUNCTIONS = (nn.functional.mse_loss,)
-REGULARIZATION_FACTORS = (0.001, 0.0001)
-OPTIMIZERS = (optim.SGD, optim.Adam)
-LEARNING_EPOCHS = (100, 200, 300)
-LEARNING_RATE_SCHEDULERS = (None, optim.lr_scheduler.CosineAnnealingLR)
-EMSEMBLE_COUNTS = (5,)
-NORMALIZERS = (nn.BatchNorm1d,)
+
+@dataclass
+class HyperParameters:
+    from torch import nn
+    from torch.optim import lr_scheduler
+    from torch import Tensor
+    from typing import Callable
+
+    HIDDEN_NODES: tuple[int, ...] = (
+        5,
+        8,
+        11,
+    )
+    ACTIVATION_FUNCTIONS: tuple[nn.Module, ...] = (nn.Tanh, nn.ReLU)
+    WEIGHT_INITIALIZERS: tuple[Callable[[Tensor], Tensor], ...] = (
+        nn.init.normal_,
+        nn.init.xavier_normal_,
+        nn.init.kaiming_normal_,
+    )
+    LOSS_FUNCTIONS: tuple[Callable[[Tensor], Tensor], ...] = (nn.functional.mse_loss,)
+    REGULARIZATION_FACTORS: tuple[float, ...] = (0.001, 0.0001)
+    OPTIMIZERS: tuple[optim.Optimizer, ...] = (
+        optim.SGD,
+        lambda *args, **kwargs: optim.SGD(*args, **kwargs, momentum=0.9),
+        optim.Adam,
+    )
+    LEARNING_EPOCHS: tuple[int, ...] = (100, 200, 300)
+    LEARNING_RATE_SCHEDULERS: tuple[lr_scheduler.LRScheduler | None, ...] = (
+        None,
+        lr_scheduler.CosineAnnealingLR,
+    )
+    EMSEMBLE_COUNTS: tuple[int, ...] = (5,)
+    NORMALIZERS: tuple[nn.BatchNorm1d | None, ...] = (None, nn.BatchNorm1d)
