174e53b9275236b421f2deb2a7fbd036847c3fdd
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-04-02 19:58:05 +0800

    (feat): pass train and eval

--- modules/prediction/experiment.py
+++ modules/prediction/experiment.py
@@ -1,51 +1,89 @@
-from torch import nn, tensor
+from torch import no_grad, nn, tensor
 from .hyper_parameters import TrainingHyperParameters
 from ..data_construction.random_construct import random_construct
 from tqdm.auto import tqdm
 
+def request_best_device():
+    """
+    A helper function to request the best device for the current environment
+    """
+    from torch import backends, cuda, device
+
+    if cuda.is_available():
+        return device("cuda")
+    elif backends.mps.is_available():
+        return device("mps")
+    elif backends.mkldnn.is_available():
+        return device("mkldnn")
+    else:
+        return device("cpu")
+
+# Define the arguments for the experiment
+DEVICE = "cpu"
 HP = list(TrainingHyperParameters.get_all_combinations())[-1]
 DATA = random_construct(100, 30)
-X = tensor(DATA.get_column('x'))
-Y = tensor(DATA.get_column('y')).unsqueeze(1)
+DATA_EVALUATION = random_construct(25, 30)
+X = tensor(DATA.get_column("x"), device=DEVICE)
+Y = tensor(DATA.get_column("y"), device=DEVICE).unsqueeze(1)
+X_EVALUATION = tensor(DATA_EVALUATION.get_column("x"), device=DEVICE)
+Y_EVALUATION = tensor(DATA_EVALUATION.get_column("y"), device=DEVICE).unsqueeze(1)
 
+print(f"DEVICE: {DEVICE}")
+
+# Define the model
 model = nn.Sequential(
-    nn.Linear(X.size(1), HP.hidden_node_count),
-    *((HP.normalizer(HP.hidden_node_count),) if HP.normalizer else ()),
+    nn.Linear(X.size(1), HP.hidden_node_count, device=DEVICE),
+    *((HP.normalizer(HP.hidden_node_count, device=DEVICE),) if HP.normalizer else ()),
     HP.activation_function(),
-    nn.Linear(HP.hidden_node_count, Y.size(1)),
+    nn.Linear(HP.hidden_node_count, Y.size(1), device=DEVICE),
 )
 
+
 def init_weights(module: nn.Module) -> None:
+    """
+    A helper function to initialize the weights of a `torch.nn.Module`
+    """
     try:
         HP.weight_initializer(module.weight)
         nn.init.zeros_(module.bias)
-    except AttributeError:
+    except Exception:
         return
 
+
+# Initialize the weights of the model
 model.apply(init_weights)
 
-# # In PyTorch, `weight_decay` is also called L2 penalty.
-optimizer = HP.optimizer(model.parameters(), weight_decay=HP.regularization_factor)
-scheduler = HP.learning_rate_scheduler(optimizer) if HP.learning_rate_scheduler else None
+# Define the optimizer and the learning rate scheduler
+optimizer = HP.optimizer(
+    model.parameters(),
+    weight_decay=HP.regularization_factor,  # In PyTorch, `weight_decay` is also called L2 penalty.
+)
+scheduler = (
+    HP.learning_rate_scheduler(optimizer) if HP.learning_rate_scheduler else None
+)
 
-epochs = HP.learning_epochs
-for _ in tqdm(range(epochs), total=epochs):
+# Train the model
+model.train()
+for _ in tqdm(range(HP.learning_epochs), total=HP.learning_epochs):
     optimizer.zero_grad()
 
+    # Forward pass
     loss = HP.loss_function(model(X), Y)
+
+    # Backward pass
     loss.backward()
 
+    # Update the weights
     optimizer.step()
+
+    # Update the learning rate
     if scheduler:
         scheduler.step()
 
-# TrainingHyperParameters(hidden_node_count=11,
-#                 activation_function=<class 'torch.nn.modules.activation.ReLU'>,
-#                 weight_initializer=<function kaiming_normal_ at 0x1293c0040>,
-#                 loss_function=<function mse_loss at 0x129321750>,
-#                 regularization_factor=0.0001,
-#                 optimizer=<class 'torch.optim.adam.Adam'>,
-#                 learning_epochs=300,
-#                 learning_rate_scheduler=<class 'torch.optim.lr_scheduler.CosineAnnealingLR'>,
-#                 ensemble_count=5,
-#                 normalizer=<class 'torch.nn.modules.batchnorm.BatchNorm1d'>)
+# Evaluate the model
+model.eval()
+with no_grad():
+    loss_evaluation = HP.loss_function(model(X_EVALUATION), Y_EVALUATION)
+
+# Print the loss
+print(f'{{"loss": {{"train": {loss:.4f}, "eval": {loss_evaluation:.4f}}}}}')
